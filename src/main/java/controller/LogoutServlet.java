b576888c3b88028b5de24ce17fd73f7f05f0d94d
Aghasi <mailtokhachatryan96@gmail.com>
2024-01-02 12:46:58 +0400

    Add cookies

---
+++ src/main/java/controller/LogoutServlet.java
@@ -0,0 +1,26 @@
+package controller;
+
+import util.CookieUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getSession().invalidate();
+        Cookie rememberCookie = CookieUtil.getCookieByName(req.getCookies());
+
+        if (rememberCookie != null) {
+            rememberCookie.setMaxAge(0);
+            resp.addCookie(rememberCookie);
+        }
+
+        resp.sendRedirect("welcome.jsp");
+    }
+}
